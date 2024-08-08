fcd47b5d3d8bd1e5fbbdb2e2592d4ca05de0c7ff
Ronnie Wanjala <ranjala90@gmail.com>
2024-08-08 15:33:39 +0300

    update home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
+function App(){
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <Outlet />
+        </>
+    );
 };
-
-export default App;
